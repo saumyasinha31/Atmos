c6ab0afceb4a793fcfff12ca8b516f92d0fc289d
SAUMYA SINHA <saumyasinha269@gmail.com>
2024-06-22 02:39:33 +0530

    update

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:mausam/Activity/home.dart';
+import 'package:mausam/Activity/location.dart';
+import 'package:mausam/Activity/loading.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: Home(),
+    
+  ));
+}
